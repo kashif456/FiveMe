009d18efbb27c80ae32a42337e97017298ad2607
Kashif Devani <KashifDevani@gmail.com>
2015-11-25 11:01:54 -0600

    add cohort controller

---
+++ app/helpers/cohortions_helper.rb
@@ -0,0 +1,2 @@
+module CohortionsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'cohortions/index'
+
+  get 'cohortions/show'
+
+  get 'cohortions/edit'
+
+  get 'cohortions/create'
+
+  get 'cohortions/update'
+
+  get 'cohortions/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
