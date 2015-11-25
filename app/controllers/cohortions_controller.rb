c88d9eb7c6c03c5dca9a3c0702a72b4579fc0646
Kashif Devani <KashifDevani@gmail.com>
2015-11-25 12:25:22 -0600

    add views for cohort

--- app/controllers/cohortions_controller.rb
+++ app/controllers/cohortions_controller.rb
@@ -7,15 +7,40 @@ class CohortionsController < ApplicationController
     @cohortion = Cohortion.find(params[:id])
   end
 
-  def edit
+  def new
+    @cohortion = Cohortion.new
   end
 
   def create
+    @cohortion = Cohortion.new(cohortion_params)
+    if @cohortion.save
+      redirect_to action: :show, id: @cohortion.id
+    else
+      redirect_to action: :new
+    end
   end
 
+  def edit
+    @cohortion = Cohortion.find(params[:id])
+  end
+
+
+ 
   def update
+     @cohortion = Cohortion.find(params[:id])
+      if @cohortion.update_attributes(cohortion_params)
+        redirect_to action: :show, id: @cohortion.id
+      else
+        redirect_to action: :update, id: @cohortion.id
+      end
   end
 
-  def new
-  end
+  private
+    def cohortion_params
+      params.require(:cohortion).permit(
+          :first, :last, :about
+        )
+    end
+
+
 end
